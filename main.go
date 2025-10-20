747c11e53d901959474dbe78dc48f903f38389d5
Kijima Daigo <norimaking777@gmail.com>
2025-10-20 18:49:08 +0900

    style: linter warning

--- main.go
+++ main.go
@@ -1,10 +1,14 @@
+// Package main is the entry point of the application.
 package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
-	fmt.Fprintln(os.Stdout, "Hello world")
+	if _, err := fmt.Fprintln(os.Stdout, "Hello world"); err != nil {
+		log.Fatal(err)
+	}
 }
